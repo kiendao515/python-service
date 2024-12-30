c8bdd03030ceaf81e7aef3869b762ba3286c6a05
Dao Trung Kien <kiendt120@ghtk.co>
2024-12-30 11:51:31 +0700

    update connect db

--- app/configs/db.py
+++ app/configs/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://kiendao:kiendao2001@cluster0.bnqgz.mongodb.net/bookstore"
-DATABASE_NAME = "bookstore"
+MONGO_URI = "mongodb://root:kiendao2001@localhost:27017/box-book-store?authSource=admin"
+DATABASE_NAME = "box-book-store"
 
 
 def get_db():
